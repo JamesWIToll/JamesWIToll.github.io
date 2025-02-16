a3f925cec688b3e0098efe6d10c7c83fb29d8e3c
wesley <james.w.tolliver@gmail.com>
2025-02-15 21:41:18 -0500

    fixed up some styling

--- src/App.vue
+++ src/App.vue
@@ -23,32 +23,33 @@ const contactShow = () => {
   filter: brightness(3) drop-shadow(1px 2px 5px lightblue);
 }
 
+
 </style>
 
 <template>
-  <BNavbar fixed="top" toggleable="lg" variant="primary">
-
-      <a href="/" style="background: transparent !important;"><img src="/icoicon.png" class="icon" alt="icon"/></a>
-      <BNavbarBrand href="/">
-        <h4 class="emphasis">James Wesley Tolliver </h4> <h5>Systems Programmer</h5>
-      </BNavbarBrand>
-      <BNavbarToggle target="nav-collapse" class="ms-auto" href="/"/>
-      <BCollapse id="nav-collapse" isNav="true" >
-        <BNavbarNav class="ms-auto" style="font-size: large">
-          <BNavItem href="/"> About Me </BNavItem>
-          <BNavItem href="/#skills"> Skills </BNavItem>
-          <BNavItem href="/#projects"> Software Projects </BNavItem>
-          <BNavItem href="/#experience"> Education </BNavItem>
-          <BNavItem href="/#experience"> Experience </BNavItem>
-          <BNavItem href="/#Art">Art</BNavItem>
-          <BNavItem @click="contactShow()"> Contact </BNavItem>
-          <BNavItem href="">Resume</BNavItem>
-        </BNavbarNav>
-      </BCollapse>
-
+  <BNavbar toggleable="lg" variant="primary">
+    <a href="/" style="background: transparent !important;"><img src="/icoicon.png" class="icon" alt="icon"/></a>
+    <BNavbarBrand href="/">
+      <h4 class="emphasis">James Wesley Tolliver </h4> <h5>Systems Programmer</h5>
+    </BNavbarBrand>
+    <BNavbarToggle target="nav-collapse" class="ms-auto" href="/"/>
+    <BCollapse id="nav-collapse" isNav="true" >
+      <BNavbarNav class="ms-auto" style="font-size: large">
+        <BNavItem href="/"> About Me </BNavItem>
+        <BNavItem href="/#skills"> Skills </BNavItem>
+        <BNavItem href="/#projects"> Software Projects </BNavItem>
+        <BNavItem href="/#experience"> Education </BNavItem>
+        <BNavItem href="/#experience"> Experience </BNavItem>
+        <BNavItem href="/#Art">Art</BNavItem>
+        <BNavItem @click="contactShow()"> Contact </BNavItem>
+        <BNavItem href="">Resume</BNavItem>
+      </BNavbarNav>
+    </BCollapse>
   </BNavbar>
 
 
-  <RouterView/>
+  <div class="container-fluid" style="margin-top: 5%;">
+    <RouterView/>
+  </div>
 
 </template>
