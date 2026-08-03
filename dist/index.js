f61fee8706dba044a517a231d7e0059110cbf67f
Wesley Tolliver <56172700+JamesWIToll@users.noreply.github.com>
2026-08-03 00:38:12 -0400

    Added content to remaining pages

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ import { Page, PageContent } from "./page.js";
 let page = new Page([
     new PageContent("about", "About Me", "#about-page-content"),
     new PageContent("career", "Career History", "#career-page-content"),
+    new PageContent("education", "Education", "#education-page-content"),
     new PageContent("projects", "My Projects", "#projects-page-content"),
     new PageContent("resume", "Resume & Links", "#resume-page-content"),
 ]);
